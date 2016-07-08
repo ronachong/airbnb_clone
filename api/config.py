464031f181d90dee6acfc7fdf0486eae1959597c
Rona Chong <ronachong@gmail.com>
2016-07-08 21:33:20 +0000

    Refactor config.py to create python vars instead of env vars.
    See previous commit for explanation.
    
    Not sure if all values should be of type string or if True/False
    and numbers should be non-string values.

--- api/config.py
+++ api/config.py
@@ -2,27 +2,28 @@ import os
 
 env = os.environ.get('AIRBNB_ENV')
 
-dev_db_hash = {'host': '158.69.79.94',
-               'user': 'airbnb_user_dev',
-               'database': 'airbnb_dev',
-               'port': '3306',
-               'charset': 'utf8',
-               'password': 'wrdev'}
-
-prod_db_hash = {'host': '158.69.79.94',
-                'user': 'airbnb_user_prod',
-                'database': 'airbnb_prod',
-                'port': '3306',
-                'charset': 'utf8',
-                'password': 'wrprod'}
-
-dev_envs = {'DEBUG': 'True', 'HOST': 'localhost', 'PORT': '3333', 'DATABASE': dev_db_hash}
-prod_envs = {'DEBUG': 'False', 'HOST': '0.0.0.0', 'PORT': '3000', 'DATABASE': prod_db_hash}
-
 if env == 'development':
     # assign specs for dev env
-    for key in dev_envs.keys(): os.environ[key] = dev_envs[key]
+    os.environ['AIRBNB_DATABASE_PWD_DEV'] = 'wrdev' 
+    DEBUG = True
+    HOST = 'localhost'
+    PORT = 3333
+    DATABASE = { 'host': '158.69.79.94',
+                 'user': 'airbnb_user_dev',
+                 'database': 'airbnb_dev',
+                 'port': '3306',
+                 'charset': 'utf8',
+                 'password': os.environ.get('AIRBNB_DATABASE_PWD_DEV') }
 
 elif env == 'production':
     # assign specs for prod env
-    for key in prod_envs.keys(): os.environ[key] = prod_envs[key]
+    os.environ['AIRBNB_DATABASE_PWD_PROD'] = 'wrprod' 
+    DEBUG = False
+    HOST = 0.0.0.0
+    PORT = 3000
+    DATABASE = { 'host': '158.69.79.94',
+                 'user': 'airbnb_user_prod',
+                 'database': 'airbnb_prod',
+                 'port': '3306',
+                 'charset': 'utf8',
+                 'password': os.environ.get('AIRBNB_DATABASE_PWD_PROD') }
