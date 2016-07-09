8428238c9d926873c66e22fd466617e15b469bb7
Rona Chong <ronachong@gmail.com>
2016-07-09 21:16:54 +0000

    Write preliminary iteration of base model for Peewee classes.
    See comment in file for purpose.

---
+++ api/app/models/base.py
@@ -0,0 +1,22 @@
+import peewee
+from config import DATABASE
+
+database = MySQLDatabase( DATABASE['database'],
+                          user=DATABASE['user'],
+                          charset=DATABASE['charset'],
+                          host=DATABASE['host'],
+                          port=DATABASE['3306'],
+                          passwd=DATABASE['password'] )
+
+class BaseModel(peewee.Model):
+    id = peewee.PrimaryKeyField(unique=True)
+    created_at = peewee.DateTimeField(default=datetime.datetime.now, formats='%Y/%m/%d %H:%M:%S') # peewee docs have datetime.datetime.now in exp; might have to check if format comes out right
+    updated_at = peewee.DateTimeField(default=datetime.datetime.now, formats='%Y/%m/%d %H:%M:%S')
+    
+    def save(self, *args, **kwargs):
+        self.updated_at = datetime.datetime.now
+        peewee.Model.save() # not sure if this goes before or after self.updated_at assignment    
+
+    class Meta:
+        database = database
+        order_by = ("id", ) # what is the extra space for?
