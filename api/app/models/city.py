3e64cd4307a2754d7ee67af6de9110d5727a7470
Rona Chong <ronachong@gmail.com>
2016-07-09 16:39:29 -0700

    Fix delete on cascade parameter for city attribute.
    cascade=True is old syntax and deprecated.
    delete_on=cascade is the new format to use.

--- api/app/models/city.py
+++ api/app/models/city.py
@@ -1,5 +1,6 @@
 from base import *
+from state import State
 
 class City(BaseModel):
     name = peewee.CharField(128, null=False, unique=True)
-    state = peewee.ForeignKeyField(State, related_name="cities", cascade=True)
+    state = peewee.ForeignKeyField(State, related_name="cities", on_delete='cascade')
