962c1084cc5e14ce806573e268b02ab02f2643ce
Rona Chong <ronachong@gmail.com>
2016-07-16 17:41:07 -0700

    Create GET, POST, and DELETE handlers for amenities and amenity.

--- api/app/models/migrate.py
+++ api/app/models/migrate.py
@@ -13,7 +13,7 @@ from flask import jsonify
 database.connect()
 
 # populate databases with tables
-#User.drop_table(cascade=True)
+Amenity.drop_table(cascade=True)
 database.create_tables([User, State, City, Place, PlaceBook, Amenity, PlaceAmenities], safe=True)
 
 # create a test entry
@@ -40,13 +40,17 @@ database.create_tables([User, State, City, Place, PlaceBook, Amenity, PlaceAmeni
 # for city in State.get(State.id == 3).cities:
 #     print city.name
 
-record = Place( owner='foo',
-                city=3,
-                name=place_name,
-                description=place_desc,
-                number_rooms=nb_rooms,
-                max_guest=place_mguests,
-                price_by_night=place_pbn,
-                latitude=place_lat,
-                longitude=place_long )
+# record = Place( owner='foo',
+#                 city=3,
+#                 name=place_name,
+#                 description=place_desc,
+#                 number_rooms=nb_rooms,
+#                 max_guest=place_mguests,
+#                 price_by_night=place_pbn,
+#                 latitude=place_lat,
+#                 longitude=place_long )
+# record.save()
+
+record = Amenity( name = 'name' )
 record.save()
+print record.to_hash()
--- api/app/models/place_amenity.py
+++ api/app/models/place_amenity.py
@@ -2,7 +2,12 @@ from peewee import Model
 from playhouse.fields import ManyToManyField
 from place import Place
 from amenity import Amenity
+from base import database
 
 class PlaceAmenities(Model):
     place = ManyToManyField(Place)
     amenity = ManyToManyField(Amenity)
+
+    class Meta:
+        database = database
+        order_by = ("id", ) # what is the extra space for?
