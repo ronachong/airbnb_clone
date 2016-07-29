0218ea28546c9d37ac26fd6ef1e8a4abc248341a
Rona Chong <ronachong@gmail.com>
2016-07-29 13:31:27 -0700

    Revise __all__ of view module init file file.
    Add all view files to __all__ variable, such that "from app.views
    import *" actually imports all of the views, and not just index.
    
    Remove redundant import line from app.py, now that import * line
    functions as intended.

--- api/app.py
+++ api/app.py
@@ -1,6 +1,5 @@
 from app import app
 from app.views import *
-from app.views import user, state, city, place, place_book, amenity
 from config import HOST, PORT, DEBUG
 
 # run app, but only if this file is being run directly (and not via another file)
