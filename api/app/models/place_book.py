b71516fe384b8472d2f5c208b3373423c2169dbb
Rona Chong <ronachong@gmail.com>
2016-07-30 21:04:48 -0700

    Troubleshoot error with placebook view.

--- api/app/models/place_book.py
+++ api/app/models/place_book.py
@@ -6,7 +6,7 @@ class PlaceBook(BaseModel):
     place = peewee.ForeignKeyField(Place)
     user = peewee.ForeignKeyField(User, related_name="places_booked")
     is_validated = peewee.BooleanField(default=False)
-    date_start = peewee.DateTimeField(null=False)
+    date_start = peewee.DateTimeField(null=False, formats='%d/%m/%Y %H:%M:%S')
     number_nights = peewee.IntegerField(default=1)
 
     def to_hash(self):
@@ -17,6 +17,6 @@ class PlaceBook(BaseModel):
         hash["place_id"] = self.place.id
         hash["user_id"] = self.user.id
         hash["is_validated"] = self.is_validated
-        hash["date_start"] = self.date_start
+        hash["date_start"] = self.date_start.strftime('%d/%m/%Y %H:%M:%S')
         hash["number_nights"] = self.number_nights
         return hash
