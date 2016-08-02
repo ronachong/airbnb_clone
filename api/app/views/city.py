70d4ad796fd12f0ad9c32054be766b094e7dab08
Rona Chong <ronachong@gmail.com>
2016-08-02 13:36:52 -0700

    Format code in city view to better conform to PEP.
    Refactor docstrings (use triple quotes, no spaces, period at end of  …
    first sentence, under 80 chars).
    Reorder imports for clarity (3rd party modules on top, imports of
    local source code below).
    Add comments to document code better.

--- api/app/views/city.py
+++ api/app/views/city.py
@@ -1,12 +1,21 @@
 from flask import jsonify, request
 from flask_json import json_response
+from peewee import *
+
 from app.models.city import City
 from app import app
-from peewee import *
 
 @app.route('/states/<state_id>/cities', methods=['GET','POST'])
 def city(state_id):
-    ''' city returns a list of all cities in the database where state = state_id in the case of a GET request, and creates a new city in the database in the case of a POST request '''
+    """Handle GET and POST requests to /states/<state_id>/cities route.
+
+    Return a list of all cities in state (according to database) in the case of
+    a GET request.
+    Create a new city record in the given state in the database in the case of a
+    POST request.
+    """
+    # handle GET requests:
+    # --------------------------------------------------------------------------
     if request.method == 'GET':
         list = []
         for record in City.select().where(City.state == state_id):
@@ -14,6 +23,8 @@ def city(state_id):
             list.append(hash)
         return jsonify(list)
 
+    # handle POST requests:
+    # --------------------------------------------------------------------------
     elif request.method == 'POST':
         try:
             record = City(name=request.form["name"], state=state_id)
@@ -31,7 +42,12 @@ def city(state_id):
 
 @app.route('/states/<state_id>/cities/<city_id>', methods=['GET','DELETE'])
 def city_id(state_id, city_id):
-    ''' '''
+    """Handle GET and DELETE requests to /states/<state_id>/cities/<city_id>
+    route.
+
+    Return a hash of the appropriate record in the case of a GET request.
+    Delete appropriate record in case of DELETE request.
+    """
     # check whether resource exists:
     # --------------------------------------------------------------------------
     try:
@@ -48,9 +64,11 @@ def city_id(state_id, city_id):
 
     # if exception does not arise:
     # --------------------------------------------------------------------------
+    # handle GET requests
     if request.method == 'GET':
         return jsonify(record.to_hash())
 
+    # handle DELETE requests
     elif request.method == "DELETE":
         record.delete_instance()
         record.save()
