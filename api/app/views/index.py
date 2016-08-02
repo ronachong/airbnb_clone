3486ed582db51a2e8625708790c89bf1bac45fe7
Rona Chong <ronachong@gmail.com>
2016-08-02 12:32:28 -0700

    Format code in index view to better conform to PEP.
    Refactor docstrings (use triple quotes, no spaces, period at end of
    first sentence, under 80 chars).
    Remove unnnecessary imports (not used by code).
    Make local_tz a local variable to utc_to_local method, and not
    global.
    Format code such that each line is under 80 chars.

--- api/app/views/index.py
+++ api/app/views/index.py
@@ -1,34 +1,45 @@
-from flask_json import FlaskJSON, json_response
-from datetime import datetime, tzinfo
+from datetime import datetime
 from pytz import utc, timezone
-from app import app, models
+
+from flask_json import json_response
 from peewee import *
 
-local_tz = timezone('America/Los_Angeles')
+from app import app, models
+
 
 @app.route('/', methods=['GET'])
 def index():
-    ''' index serves a hash with the status ok and local and utc times for any requests with status 200 '''
+    """Serve a simple hash for any requests to / with status 200."""
     return json_response(
         status="OK",
         utc_time=datetime.utcnow().strftime('%d/%m/%Y %H:%M:%S'),
         time=utc_to_local(datetime.utcnow()).strftime('%d/%m/%Y %H:%M:%S')
     )
 
+
 @app.errorhandler(404)
 def not_found(e):
-    ''' not_found serves a json hash with code 404 and msg "not found" for any requests with status 404 '''
-    return json_response(add_status_=False, status_=404, code=404, msg="not found")
+    """Serve a 404, not found json for any requests to / with status 404."""
+    return json_response(
+        add_status_=False,
+        status_=404,
+        code=404,
+        msg="not found"
+    )
+
 
 def utc_to_local(utc_dt):
-    ''' utc_to_local converts a utc date time to the specified local timezone local_tz '''
+    """Convert a utc date time to the specified local timezone local_tz."""
+    local_tz = timezone('America/Los_Angeles')
     local_dt = utc_dt.replace(tzinfo=utc).astimezone(local_tz)
     return local_tz.normalize(local_dt)
 
+
 def before_request():
-    ''' before_request specifies that the database should be connected to rior to each request '''
+    """Connect to airbnb_test to prior to each request."""
     models.database.connect()
 
+
 def after_request():
-    ''' after_request specifies that the connection to the database should be closed after each request '''
+    """Close connection to the database after each request."""
     models.database.close()
