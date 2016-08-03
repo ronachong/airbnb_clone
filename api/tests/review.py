d258ca312d0f09708299561a8a82abf26e28fd19
Rona Chong <ronachong@gmail.com>
2016-08-03 13:45:19 -0700

    Create tests for GETs and POSTs to /places/<place_id>/reviews.
    Using tests written for /users/<user_id>/reviews as template.

--- api/tests/review.py
+++ api/tests/review.py
@@ -112,12 +112,12 @@ class reviewTestCase(unittest.TestCase):
 
         return POST_request
 
-"""Tests for place reviews."""
+"""Tests for user reviews."""
 
     def subtest_createWithAllParams_uroute(self):
         """
-        Test proper creation of an review record upon POST request to the API
-        with all parameters provided.
+        Test proper creation of a user review record upon POST request to the
+        API with all parameters provided.
         """
         POST_request = self.app.post('/users/1/reviews', data=dict(
             message='foo-message',
@@ -136,8 +136,8 @@ class reviewTestCase(unittest.TestCase):
 
     def subtest_createWithoutAllParams_uroute(self):
         """
-        Test proper non-creation (or creation) of an review in all cases of a
-        parameter missing in POST request to the API.
+        Test proper non-creation (or creation) of a user review in all cases of
+        a parameter missing in POST request to the API.
         """
         # test that request missing optional stars param is handled w/ success
         # ----------------------------------------------------------------------
@@ -146,7 +146,7 @@ class reviewTestCase(unittest.TestCase):
         ))
         self.assertEqual(POST_request2.status[:3], '200')
 
-        # test that request missing optional stars param fails
+        # test that request missing mandatory message param fails
         # ----------------------------------------------------------------------
         POST_request3 = self.app.post('/users/1/reviews', data=dict(
             stars=5
@@ -155,7 +155,7 @@ class reviewTestCase(unittest.TestCase):
 
     def test_create_uroute(self):
         """
-        Test proper creation (or non-creation) of review records upon POST
+        Test proper creation (or non-creation) of user review records upon POST
         requests to API.
         """
         # test response to POST request to user ID which does not exist
@@ -181,8 +181,8 @@ class reviewTestCase(unittest.TestCase):
 
     def test_list_uroute(self):
         """
-        Test proper representation of all review records upon GET requests to
-        API.
+        Test proper representation of user review records upon GET requests
+        to API.
         """
         # test response to GET request by user ID which does not exist
         # ----------------------------------------------------------------------
@@ -191,17 +191,17 @@ class reviewTestCase(unittest.TestCase):
 
         # test response to GET request by user ID which exists
         # ----------------------------------------------------------------------
-        GET_request1 = self.app.get('users/1/reviews')
-        self.assertEqual(len(json.loads(GET_request1.data)), 0)
+        GET_request2 = self.app.get('users/1/reviews')
+        self.assertEqual(len(json.loads(GET_request2.data)), 0)
 
         self.createReviewViaPeewee()
 
-        GET_request2 = self.app.get('users/1/reviews')
-        self.assertEqual(len(json.loads(GET_request2.data)), 1)
+        GET_request3 = self.app.get('users/1/reviews')
+        self.assertEqual(len(json.loads(GET_request3.data)), 1)
 
     def test_get_uroute(self):
         """
-        Test proper representation of an review record upon GET requests
+        Test proper representation of a user review record upon GET requests
         via review ID to API.
         """
         # set-up for tests
@@ -240,16 +240,14 @@ class reviewTestCase(unittest.TestCase):
 
     def test_delete_uroute(self):
         """
-        Test deletion of review records upon DELETE requests to API.
+        Test deletion of user review records upon DELETE requests to API.
         """
         # test response to DELETE request for review by review id
         # ----------------------------------------------------------------------
         self.createReviewViaPeewee()
 
         GET_request1 = self.app.get('users/1/reviews')
-
         DELETE_request1 = self.app.delete('/users/1/1')
-
         GET_request2 = self.app.get('users/1/reviews')
 
         num_records_b4 = len(json.loads(GET_request1.data))
@@ -273,92 +271,90 @@ class reviewTestCase(unittest.TestCase):
 
 """Tests for place reviews."""
 
+    def subtest_createWithAllParams_proute(self):
+        """
+        Test proper creation of a place review record upon POST request to the
+        API with all parameters provided.
+        """
+        POST_request = self.app.post('/places/1/reviews', data=dict(
+            message='foo-message',
+            # user_id omitted since req. handler should get from uri
+            stars=5
+        ))
+        self.assertEqual(POST_request1.status[:3], '200')
 
-    #
-    # def subtest_createWithAllParams_proute(self):
-    #     """
-    #     Test proper creation of an review record upon POST request to the API
-    #     with all parameters provided.
-    #     """
-    #     POST_request = self.app.post('/users/1/reviews', data=dict(
-    #         message='foo-message',
-    #         # user_id omitted since req. handler should get from uri
-    #         stars=5
-    #     ))
-    #     self.assertEqual(POST_request1.status[:3], '200')
-    #
-    #     now = datetime.now().strftime('%d/%m/%Y %H:%M')
-    #
-    #     self.assertEqual(Review.get(Review.id == 1).message, 'foo-message')
-    #     self.assertEqual(Review.get(Review.id == 1).user.id, 1)
-    #     self.assertEqual(Review.get(Review.id == 1).stars, 5)
-    #     self.assertEqual(Review.get(Review.id == 1).created_at.strftime('%d/%m/%Y %H:%M'), now)
-    #     self.assertEqual(Review.get(Review.id == 1).updated_at.strftime('%d/%m/%Y %H:%M'), now)
-    #
-    # def subtest_createWithoutAllParams_proute(self):
-    #     """
-    #     Test proper non-creation (or creation) of an review in all cases of a
-    #     parameter missing in POST request to the API.
-    #     """
-    #     # test that request missing optional stars param is handled w/ success
-    #     # ----------------------------------------------------------------------
-    #     POST_request2 = self.app.post('/users/1/reviews', data=dict(
-    #         message='foo-message'
-    #     ))
-    #     self.assertEqual(POST_request2.status[:3], '200')
-    #
-    #     # test that request missing optional stars param fails
-    #     # ----------------------------------------------------------------------
-    #     POST_request3 = self.app.post('/users/1/reviews', data=dict(
-    #         stars=5
-    #     ))
-    #     self.assertEqual(POST_request3.status[:3], '400')
-    #
-    # def test_create_proute(self):
-    #     """
-    #     Test proper creation (or non-creation) of review records upon POST
-    #     requests to API.
-    #     """
-    #     # test response to POST request to user ID which does not exist
-    #     # ----------------------------------------------------------------------
-    #     xPOST_request = self.app.post('/users/1000/reviews', data=dict(
-    #         message='foo-message',
-    #         # user_id omitted since req. handler should get from uri
-    #         stars=5
-    #     ))
-    #     self.assertEqual(len(json.loads(xPOST_request.data)), 404)
-    #
-    #     # test creation of review with all parameters provided in POST request
-    #     # ----------------------------------------------------------------------
-    #     self.subtest_createWithAllParams_uroute()
-    #
-    #     # test that review ID for sole record in database is correct
-    #     # ----------------------------------------------------------------------
-    #     self.assertEqual(Review.select().get().id, 1)
-    #
-    #     # test creatxn of review in all cases of parameter missing in POST req.
-    #     # ----------------------------------------------------------------------
-    #     self.subtest_createWithoutAllParams_uroute()
-    #
-    # def test_list_proute(self):
-    #     """
-    #     Test proper representation of all review records upon GET requests to
-    #     API.
-    #     """
-    #     # test response to GET request by user ID which does not exist
-    #     # ----------------------------------------------------------------------
-    #     GET_request1 = self.app.get('users/1000/reviews')
-    #     self.assertEqual(len(json.loads(GET_request1.data)), 404)
-    #
-    #     # test response to GET request by user ID which exists
-    #     # ----------------------------------------------------------------------
-    #     GET_request1 = self.app.get('users/1/reviews')
-    #     self.assertEqual(len(json.loads(GET_request1.data)), 0)
-    #
-    #     self.createReviewViaPeewee()
-    #
-    #     GET_request2 = self.app.get('users/1/reviews')
-    #     self.assertEqual(len(json.loads(GET_request2.data)), 1)
+        now = datetime.now().strftime('%d/%m/%Y %H:%M')
+
+        self.assertEqual(Review.get(Review.id == 1).message, 'foo-message')
+        self.assertEqual(Review.get(Review.id == 1).place.id, 1)
+        self.assertEqual(Review.get(Review.id == 1).stars, 5)
+        self.assertEqual(Review.get(Review.id == 1).created_at.strftime('%d/%m/%Y %H:%M'), now)
+        self.assertEqual(Review.get(Review.id == 1).updated_at.strftime('%d/%m/%Y %H:%M'), now)
+
+    def subtest_createWithoutAllParams_proute(self):
+        """
+        Test proper non-creation (or creation) of a place review in all cases
+        of a parameter missing in POST request to the API.
+        """
+        # test that request missing optional stars param is handled w/ success
+        # ----------------------------------------------------------------------
+        POST_request2 = self.app.post('/places/1/reviews', data=dict(
+            message='foo-message'
+        ))
+        self.assertEqual(POST_request2.status[:3], '200')
+
+        # test that request missing mandatory message param fails
+        # ----------------------------------------------------------------------
+        POST_request3 = self.app.post('/place/1/reviews', data=dict(
+            stars=5
+        ))
+        self.assertEqual(POST_request3.status[:3], '400')
+
+    def test_create_proute(self):
+        """
+        Test proper creation (or non-creation) of place review records upon
+        POST requests to API.
+        """
+        # test response to POST request to place ID which does not exist
+        # ----------------------------------------------------------------------
+        xPOST_request = self.app.post('/place/1000/reviews', data=dict(
+            message='foo-message',
+            # user_id omitted since req. handler should get from uri
+            stars=5
+        ))
+        self.assertEqual(len(json.loads(xPOST_request.data)), 404)
+
+        # test creation of review with all parameters provided in POST request
+        # ----------------------------------------------------------------------
+        self.subtest_createWithAllParams_uroute()
+
+        # test that review ID for sole record in database is correct
+        # ----------------------------------------------------------------------
+        self.assertEqual(Review.select().get().id, 1)
+
+        # test creatxn of review in all cases of parameter missing in POST req.
+        # ----------------------------------------------------------------------
+        self.subtest_createWithoutAllParams_uroute()
+
+    def test_list_proute(self):
+        """
+        Test proper representation of all place review records upon GET
+        requests to API.
+        """
+        # test response to GET request by place ID which does not exist
+        # ----------------------------------------------------------------------
+        GET_request1 = self.app.get('places/1000/reviews')
+        self.assertEqual(len(json.loads(GET_request1.data)), 404)
+
+        # test response to GET request by user ID which exists
+        # ----------------------------------------------------------------------
+        GET_request2 = self.app.get('places/1/reviews')
+        self.assertEqual(len(json.loads(GET_request2.data)), 0)
+
+        self.createReviewViaPeewee()
+
+        GET_request3 = self.app.get('places/1/reviews')
+        self.assertEqual(len(json.loads(GET_request3.data)), 1)
     #
     # def test_get_proute(self):
     #     """
