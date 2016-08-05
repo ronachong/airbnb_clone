2554c32d6a667cc5baaec5d540870f5fb114f993
Rona Chong <ronachong@gmail.com>
2016-08-05 16:46:48 -0700

    Create Review model and to_hash method based on provided specs.
    per Project 186 task 5:
    class Review inherits from BaseModel:
    - message, text, not null
    - stars, integer, default 0
    - user, foreign key to User, related_name = "reviews", delete on cascade. (A
    user can have multiple reviews, but a review is linked to only one user)
    
    - a method def to_hash(self): to return a hash with those elements: id,
    created_at, updated_at, message, stars, fromuserid (linked to user.id),
    to_user_id (linked to ReviewUser table ; can be null), to_place_id (linked to
    ReviewPlace table ; can be null)

---
+++ api/app/models/review.py
@@ -0,0 +1,31 @@
+from base import *
+from review_user import ReviewUser
+from review_place import ReviewPlace
+
+class Review(BaseModel):
+    message = peewee.TextField(null=False)
+    stars = peewee.IntegerField(default=0)
+    user = peewee.ForeignKeyField(User, related_name="reviews", on_delete='cascade')
+
+    def to_hash(self):
+        hash = {}
+        hash["id"] = self.id
+        hash["created_at"] = self.created_at.strftime('%d/%m/%Y %H:%M:%S')
+        hash["updated_at"] = self.updated_at.strftime('%d/%m/%Y %H:%M:%S')
+        hash["message"] = self.message
+        hash["stars"] = self.stars
+        hash["from_user_id"] = self.user.id
+
+        try:
+            review_user = ReviewUser.get(ReviewUser.review.id == self.id)
+            hash["to_user_id"] = review_user.user.id
+        except:
+            hash["to_user_id"] = None
+
+        try:
+            review_place = ReviewPlace.get(ReviewPlace.review.id == self.id)
+            hash["to_place_id"] = review_place.place.id
+        except:
+            hash["to_user_id"] = None
+
+        return hash
