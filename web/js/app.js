778745795f819fda7a3fc04a9317a3e4f94a1d31
Rona Chong <ronachong@gmail.com>
2016-10-05 16:37:15 -0700

    Create var for web app.

--- web/js/app.js
+++ web/js/app.js
@@ -1,1 +1,33 @@
-console.log("test")
+console.log("loading app root")
+
+import React from 'react';
+import ReactDom from 'react-dom';
+
+import Header from './components/header.js';
+
+const styles = {
+  header: {
+    height: '60px',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+    //flexWrap: 'nowrap'
+  }
+}
+
+var Site = React.createClass({
+  getInitialState: function() {
+    return {
+      title: 'fairbnb'
+    }
+  },
+  render: function () {
+    return (
+      <div>
+        <Header />
+      </div>
+    )
+  }
+});
+
+ReactDom.render(<Site />, document.getElementById('app'));
